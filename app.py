e6dbe582b6c43332f768f26782f4e9c4c20c0a52
Rubens Santos <contato.rubenscsantos@gmail.com>
2019-10-01 04:21:42 -0300

    Some Changes

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
+
 ### swagger specific ###
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.json'
+
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+        'app_name': "Assessment Movie API"
     }
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
